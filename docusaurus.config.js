7e235587baf2bb5205234b037b78531af8a200ec
Summer <71572746+SummerSigh@users.noreply.github.com>
2024-08-27 23:03:34 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://www.babble.diy/',
+  url: 'https://docs.babble.diy/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',

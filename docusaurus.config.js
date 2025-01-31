0a4cef18f08429c63d2966cb180256f911d8f55a
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2025-01-31 10:57:21 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,7 +80,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          // {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/SummerSigh/ProjectBabble',
             label: 'GitHub',
@@ -120,10 +120,10 @@ const config = {
           {
             title: 'More',
             items: [
-              // {
-              //   label: 'Blog',
-              //   to: '/blog',
-              // },
+              {
+               label: 'Blog',
+               to: '/blog',
+              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/Project-Babble/ProjectBabble',

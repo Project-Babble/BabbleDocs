0d96e021439fd050033b4ffe280c98360423d048
Vixie <69331795+NeonKitty72@users.noreply.github.com>
2026-02-21 18:01:23 -0600

    Fix Edit URL Link
    Previously, using the "Edit this page" link at the bottom of a page would take you to the default Docusaurus repo. Now it'll take you to the BabbleDocs GitHub repo.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,14 +45,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Project-Babble/BabbleDocs/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Project-Babble/BabbleDocs/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',

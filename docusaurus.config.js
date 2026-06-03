99a57efe5781fe05c2b14084e241244233f618ce
Summer <71572746+SummerSigh@users.noreply.github.com>
2026-06-03 16:46:01 -0700

    update broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,7 +105,7 @@ const config = {
               },
               {
                 label: 'Data Privacy',
-                to: '/docs/dataprivacy',
+                to: '/docs/dataPrivacy',
               },
             ],
           },
